e366699d9f4730ce3ae67d9ff7583c0abd50c3d5
hsn2 <912372447@qq.com>
2022-12-17 22:05:41 +0800

    add:0x24 packet

--- packet/packets/play/S0x24.py
+++ packet/packets/play/S0x24.py
@@ -1,9 +1,9 @@
-from data_types import Byte
+from data_types import Byte, Array
 from packet.base_packet import BasePacket
 
 
-class S0x2(BasePacket):
+class S0x24(BasePacket):
     def __init__(self):
         super().__init__()
-        self.packet_id = 2
-        self.fields_structure = [int, bool, Byte]
+        self.packet_id = 24
+        self.fields_structure = [int, bool, Byte, Byte, Array(str), ]
